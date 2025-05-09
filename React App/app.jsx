9c22119e5d2e10bcc3b65cd368019e47015a9eeb
Ogak <78386672+Ogak-AI@users.noreply.github.com>
2025-05-09 12:46:13 +0100

    Create app.jsx

---
+++ React App/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
