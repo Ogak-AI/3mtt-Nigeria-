d3cbdb06d319c63bb766b0d10480937d3a29ff9c
Ogak <78386672+Ogak-AI@users.noreply.github.com>
2025-05-27 10:59:14 +0100

    Create data.js

---
+++ ExpressJs/data.js
@@ -0,0 +1,6 @@
+let items = [
+    { id: 1, name: "Item One", description: "Description of Item One" },
+    { id: 2, name: "Item Two", description: "Description of Item Two" }
+];
+
+module.exports = items;
