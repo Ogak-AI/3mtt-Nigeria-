5ee5aa734231351ddefde11e494bec954d2a48c7
Ogak <78386672+Ogak-AI@users.noreply.github.com>
2025-05-28 16:52:19 +0100

    Delete ExpressJs/data.js

--- ExpressJs/data.js
+++
@@ -1,6 +0,0 @@
-let items = [
-    { id: 1, name: "Item One", description: "Description of Item One" },
-    { id: 2, name: "Item Two", description: "Description of Item Two" }
-];
-
-module.exports = items;
